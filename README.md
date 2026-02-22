4bbf61d3e49dfe5cde04b74083953f2d20c67be3
msirazu <thebdrazu@gmail.com>
2026-02-22 16:16:59 +0600

    filter toggle completed

--- README.md
+++ README.md
@@ -2,15 +2,3 @@
 <h2>Job Application Tracker</h2>
 <h3>Technology : HTML5, CSS3, Tailwind, DaisyUI, JavaScript (Vanila)</h3>
 <h4>Responsive : Yes</h4>
-
-<h5>Questions Part</h5>
-
-1. What is the difference between getElementById, getElementsByClassName, and querySelector / querySelectorAll?
-
-2. How do you create and insert a new element into the DOM?
-
-3. What is Event Bubbling? And how does it work?
-
-4. What is Event Delegation in JavaScript? Why is it useful?
-
-5. What is the difference between 'preventDefault()' and 'stopPropagation()' methods?
--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,51 @@ const totalJobs = [];
 const interviewJobs = [];
 const rejectedJobs = [];
 
-const interview = document.querySelector('.interview-btn');
-const rejected = document.querySelector('.rejected-btn');
+const interviewButton = document.querySelector('.interview-btn');
+const rejectedButton = document.querySelector('.rejected-btn');
 const cardSection = document.getElementById('card-section');
+const allJobs = document.querySelectorAll('.card-all');
+const interviewSection = document.getElementById('interview-section');
+   const rejectedSection = document.getElementById('rejected-section');
+   const allJobsSection = document.querySelector('.all-jobs-section');
+   const allFilterButton = document.getElementById('all-filter-btn');
+    const interviewFilterButton = document.getElementById('interview-filter-btn');
+    const rejectedFilterButton = document.getElementById('rejected-filter-btn');
 
-document.querySelector('.total-job-num').innerText = totalJobs.length;
+document.querySelector('.total-job-num').innerText = allJobs.length;
 document.querySelector('.interview-job-num').innerText = interviewJobs.length;
 document.querySelector('.rejected-job-num').innerText = rejectedJobs.length;
 
 cardSection.addEventListener('click', (e) => {
-    const target = e.target;
+   
 });
+
+const filterToggle = (id) => {
+   if (id === 'interview-filter-btn') {
+        interviewSection.classList.remove('hidden');
+        allJobsSection.classList.add('hidden');
+        rejectedSection.classList.add('hidden');
+
+        interviewFilterButton.classList.add('btn-primary');
+        rejectedFilterButton.classList.remove('btn-primary');
+        allFilterButton.classList.remove('btn-primary');
+
+   } else if (id === 'rejected-filter-btn') {
+        rejectedSection.classList.remove('hidden');
+        interviewSection.classList.add('hidden');
+        allJobsSection.classList.add('hidden');
+
+        rejectedFilterButton.classList.add('btn-primary');
+        interviewFilterButton.classList.remove('btn-primary');
+        allFilterButton.classList.remove('btn-primary');
+
+   } else {
+        rejectedSection.classList.add('hidden');
+        interviewSection.classList.add('hidden');
+        allJobsSection.classList.remove('hidden');
+
+        allFilterButton.classList.add('btn-primary');
+        rejectedFilterButton.classList.remove('btn-primary');
+        interviewFilterButton.classList.remove('btn-primary');
+   }
+}
