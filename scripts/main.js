fc32addda620d63b6fc36fadf504d2686e8fd0df
msirazu <thebdrazu@gmail.com>
2026-02-22 13:00:19 +0600

    total 8 cards added complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
-const interview = document.getElementById('interview-btn');
-const rejected = document.getElementById('rejected-btn');
+const totalJobs = [];
+const interviewJobs = [];
+const rejectedJobs = [];
 
-console.log(interview, rejected)
+const interview = document.querySelector('.interview-btn');
+const rejected = document.querySelector('.rejected-btn');
+const cardSection = document.getElementById('card-section');
+
+document.querySelector('.total-job-num').innerText = totalJobs.length;
+document.querySelector('.interview-job-num').innerText = interviewJobs.length;
+document.querySelector('.rejected-job-num').innerText = rejectedJobs.length;
+
+cardSection.addEventListener('click', (e) => {
+    const target = e.target;
+});
