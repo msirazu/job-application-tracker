729aa65b4394b47759fab479f9525cdf4de8eb1c
msirazu <thebdrazu@gmail.com>
2026-02-23 14:12:09 +0600

    array showed on interview tab done

--- scripts/main.js
+++ scripts/main.js
@@ -5,19 +5,20 @@ let rejectedJobsList = [];
 const cardSection = document.getElementById('card-section');
 const jobContainer = document.getElementById('jobs-container');
 const allJobs = document.querySelectorAll('.card-all');
+
 const interviewSection = document.getElementById('interview-section');
 const rejectedSection = document.getElementById('rejected-section');
 const allJobsSection = document.querySelector('.all-jobs-section');
+
 const allFilterButton = document.getElementById('all-filter-btn');
 const interviewFilterButton = document.getElementById('interview-filter-btn');
 const rejectedFilterButton = document.getElementById('rejected-filter-btn');
 
-document.querySelector('.total-job-num').innerText = allJobs.length;
+document.querySelector('.total-job-num').innerText = cardSection.children.length;
 document.querySelector('.interview-job-num').innerText = interviewJobsList.length;
 document.querySelector('.rejected-job-num').innerText = rejectedJobsList.length;
 
 jobContainer.addEventListener('click', (e) => {
-   
    if (e.target.classList.contains('interview-btn')) {
      const cardParent = e.target.parentNode.parentNode;
      const cardTitle = cardParent.querySelector('.card-title').innerText;
@@ -36,8 +37,41 @@ jobContainer.addEventListener('click', (e) => {
      if (!isDataExists) {
           interviewJobsList.push(jobsData);
      }
+     cardParent.querySelector('.apply-btn').innerText = 'interview';
+     renderInterview();
    }
 });
+
+const renderInterview = () => {
+     interviewSection.innerHTML = '';
+     for (let iJobs of interviewJobsList) {
+          const div = document.createElement('div');
+     div.classList.add('card-all', 'bg-white', 'my-5', 'rounded-lg', 'p-3', 'space-y-5');
+     div.innerHTML = `
+     <div class="flex justify-between items-center">
+                    <div>
+                    <h2 class="card-title font-semibold">${iJobs.cardTitle}</h2>
+                    <h4 class="card-title-text text-sm">${iJobs.cardTitleText}</h4>
+                    </div>
+                    <div>
+                        <button class="delete-btn bg-gray-100 flex justify-center items-center p-2 rounded-full hover:bg-gray-200 hover:cursor-pointer">
+                        <i class="fa-solid fa-delete-left text-gray-600"></i>
+                        </button>
+                    </div>
+                    </div>
+                    <div class="card-p flex gap-2 text-sm">
+<p class="card-p-1 text-[#64748B]">${iJobs.cardP}</p>
+                    </div>
+                    <button class="apply-btn uppercase bg-[#EEF4FF] py-1 px-3 text-[#002C5C]">${iJobs.status}</button>
+                    <p class="card-des text-sm">${iJobs.cardDes}</p>
+                    <div class="flex gap-2">
+                        <button class="interview-btn btn btn-accent btn-outline uppercase btn-sm">interview</button>
+                        <button class="rejected-btn btn uppercase btn-secondary btn-outline btn-sm">rejected</button>
+                    </div>
+     `
+     interviewSection.appendChild(div);
+     }
+}
 
 const filterToggle = (id) => {
    if (id === 'interview-filter-btn') {
